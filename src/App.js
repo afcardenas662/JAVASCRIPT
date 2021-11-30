450951811e39410ea993182b0a8a7cf00491004d
afcardenas662 <anafelipe.567@gmail.com>
2021-11-30 01:55:58 -0500

    segundo intento

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
+import MiComponente from './componentes/MiComponente';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+          
+          Nunca nos rendimos
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        { alert ("Bienvenidos, ya pueden entrar")}
+        
+        <div className="componentes">
+          
+          <MiComponente />
+
+
+
+        </div>
       </header>
+      
+
     </div>
   );
 }
---
+++ src/componentes/MiComponente.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+class MiComponente extends React.Component{
+    
+
+    render (){
+
+        return ( <h1>Carnero</h1>);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export default MiComponente;
