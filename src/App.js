619200cd6032472d123006d84a5229fe2c95b626
afcardenas662 <anafelipe.567@gmail.com>
2021-12-06 23:55:25 -0500

    segunda parte -.-

--- src/App.js
+++ src/App.js
@@ -1,34 +1,94 @@
-import logo from './logo.jpg';
-import './App.css';
-import MiComponente from './componentes/MiComponente';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        
-        <p>
-
-          
-          Nunca nos rendimos
-        </p>
-
-        { alert ("Bienvenidos, ya pueden entrar")}
-        
-        <div className="componentes">
-          
-          <MiComponente />
+import React, { Component } from 'react';
+import logo from '../src/assets/imagenes/logo.jpg';
+import '../src/App.css';
 
 
+import { todos } from '../src/todos.json';
 
+
+import TodoForm from '../src/componentes/TodoFrom';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  removeTodo(index) {
+    this.setState({
+      todos: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    });
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4" key={i}>
+          <div className="card mt-4">
+            <div className="card-title text-center">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              {todo.description}
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Delete
+              </button>
+            </div>
+          </div>
         </div>
-      </header>
-      
+      )
+    });
 
-    </div>
-  );
+    // RETURN THE COMPONENT
+    return (
+      <div className="App">
+
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
+              {this.state.todos.length}
+            </span>
+          </a>
+        </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+
+            <div className="col-md-4 text-center">
+                <img src={logo} className="App-logo" alt="logo" />
+              <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
+            </div>
+
+            <div className="col-md-8">
+              <div className="row">
+                {todos}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
